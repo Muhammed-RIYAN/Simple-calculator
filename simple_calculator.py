0dd3d5554e82359d0cb0e072cba7367d8aef2d5f
Muhammed-RIYAN <94755875+Muhammed-RIYAN@users.noreply.github.com>
2023-02-17 05:22:38 -0800

    Add files via upload

---
+++ simple_calculator.py
@@ -0,0 +1,18 @@
+x = 10
+y = 20 
+sum = x + y
+difference = x-y
+multiplication = x*y
+division = x/y
+remainder = x%y
+
+print("the sum of x and y is: ",sum)
+
+print("the difference of x and y is: ",difference)
+
+print("the product of x and y is: ",multiplication)
+
+print("the division of x and y is: ",division)
+
+print("the remainder of x and y is: ",remainder)
+
